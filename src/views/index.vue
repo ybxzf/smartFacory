9e942d75f85d803f6d83005e16e96d771f457b3a
1554 <517394139@qq.com>
2024-11-08 13:02:21 +0800

    首页大屏组件划分

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,7 +3,45 @@
     <div class="big-screen-header">
       <svg-icon class="full-screen-icon" :icon-class="isFullScreen ? 'exit-fullscreen' : 'fullscreen'" @click="toggleFullScreen()" />
     </div>
-
+    <div class="screen-title">标题</div>
+    <div class="screen-content">
+      <el-col class="grid-content grid-left">
+        <el-col :span="24">
+          <div class="left-item item-top" >
+            1
+          </div>
+          <div class="left-item item-middle" >
+            2
+          </div>
+          <div class="left-item item-bottom" >
+            3
+          </div>
+        </el-col>
+      </el-col>
+      <el-col class="grid-content grid-center">
+        <el-col :span="24">
+          <div class="center-item item-top" >
+            4
+          </div>
+          <div class="center-item item-bottom" >
+            5
+          </div>
+        </el-col>
+      </el-col>
+      <el-col class="grid-content grid-right">
+        <el-col :span="24">
+          <div class="right-item item-top" >
+            6
+          </div>
+          <div class="right-item item-middle" >
+            7
+          </div>
+          <div class="right-item item-bottom" >
+            8
+          </div>
+        </el-col>
+      </el-col>
+    </div>
   </div>
 </template>
 
@@ -70,21 +108,85 @@ export default {
   background-position: center center;
   /* 背景图片固定，不随滚动 */
   // background-attachment: fixed;
-  background-color: #3157ff;
+  background-color: #122A4A;
   position: relative;
+  color: #ffffff;
 
   .big-screen-header {
     position: absolute;
-    top: 10px;
-    right: 10%;
+    top: 3vh;
+    right: 5%;
     .full-screen-icon{
       color: aqua;
     }
   }
 }
 
-.big-screen-home:fullscreen {
+.big-screen-home :fullscreen {
   width: 100%;
   height: 100vh;
+}
+
+.screen-title {
+  width:100%;
+  height:7vh;
+  text-align:center;
+}
+
+.screen-content {
+  display:flex;
+  // flex-direction: column;
+  height: calc(100vh - 7vh);
+
+  .grid-content {
+    flex-grow: 1; /* 让子元素填满剩余空间 */
+  }
+  .grid-left {
+    width: 25%;
+    .left-item{
+      height:33vh;
+      width:100%;
+    }
+    .item-top{
+      background-color: rgba(91,0, 0,0.5);
+    }
+    .item-middle{
+      background-color: rgba(255,0, 0,0.5);
+    }
+    .item-bottom{
+      background-color: rgba(255,128, 128,0.5);
+    }
+
+  }
+  .grid-center {
+    width: 50%;
+    .center-item{
+      width:100%;
+    }
+    .item-top{
+      height:66vh;
+      background-color: rgba(194, 198, 57,0.5);
+    }
+    .item-bottom{
+      height:33vh;
+      background-color: rgba(255, 128, 64,0.5);
+    }
+  }
+  .grid-right {
+    width: 25%;
+    .right-item{
+      height:33vh;
+      width:100%;
+    }
+    .item-top{
+      background-color: rgba(0, 128, 255,0.5);
+    }
+    .item-middle{
+      background-color: rgba(0, 128, 192,0.5);
+    }
+    .item-bottom{
+      background-color: rgba(128, 128, 255,0.5);
+    }
+  }
 }
 </style>
