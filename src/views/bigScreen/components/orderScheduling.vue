e7dcd88f1de5ae574785433aa2ce88b5e99f5ca1
mujinzjh <1161223922@qq.com>
2024-11-14 22:55:01 +0800

    [feat]1模块进度100%

--- src/views/bigScreen/components/orderScheduling.vue
+++ src/views/bigScreen/components/orderScheduling.vue
@@ -4,7 +4,8 @@
 			<span>订单排产计划</span>
 		</div>
 		<div class="table-c">
-			<el-table :data="tableData" height="100%" style="width: 100%" header-row-class-name="table-h-bg"
+			<el-table :data="tableData" height="100%" ref="scrollTable" @mouseenter.native="handleEnter"
+				@mouseleave.native="handleLevel" style="width: 100%" header-row-class-name="table-h-bg"
 				:row-class-name="tableRowClassName">
 				<el-table-column prop="number" width="60" show-overflow-tooltip label="计单编号">
 				</el-table-column>
@@ -14,7 +15,7 @@
 				</el-table-column>
 				<el-table-column prop="finishNum" show-overflow-tooltip label="完成数量">
 				</el-table-column>
-				<el-table-column prop="status"  width="60" show-overflow-tooltip label="订单状态">
+				<el-table-column prop="status" show-overflow-tooltip label="订单状态">
 				</el-table-column>
 			</el-table>
 		</div>
@@ -82,13 +83,45 @@ export default {
 				orderNum: 1000,
 				finishNum: 200,
 				status: '未处理'
-			}]
+			}],
+			scrolltimer: null,
 		}
 	},
 	mounted() {
-
+		this.autoScroll();
+	},
+	beforeDestroy() {
+		this.autoScroll(true);
 	},
 	methods: {
+		handleLevel() {
+			this.autoScroll(false);
+		},
+		handleEnter() {
+			this.autoScroll(true);
+		},
+		autoScroll(stop) {
+			const table = this.$refs.scrollTable
+			// 拿到表格中承载数据的div元素
+			const divData = table.$refs.bodyWrapper
+			// 拿到元素后，对元素进行定时增加距离顶部距离，实现滚动效果(此配置为每100毫秒移动1像素)
+			if (stop) {
+				//再通过事件监听，监听到 组件销毁 后，再执行关闭计时器。
+				window.clearInterval(this.scrolltimer)
+			} else {
+				this.scrolltimer = window.setInterval(() => {
+					// 元素自增距离顶部1像素
+					divData.scrollTop += 1
+					// 判断元素是否滚动到底部(可视高度+距离顶部=整个高度)
+					if (divData.clientHeight + divData.scrollTop == divData.scrollHeight) {
+						// 重置table距离顶部距离
+						divData.scrollTop = 0
+						// 重置table距离顶部距离。值=(滚动到底部时，距离顶部的大小) - 整个高度/2
+						// divData.scrollTop = divData.scrollTop - divData.scrollHeight / 2
+					}
+				}, 150) // 滚动速度
+			}
+		},
 		tableRowClassName({ row, rowIndex }) {
 			return rowIndex % 2 === 0 ? 'odd-row' : 'even-row';
 		}
@@ -100,7 +133,7 @@ export default {
 .section {
 	width: 100%;
 	height: 100%;
-  padding-top: 3%;
+	padding-top: 3%;
 }
 
 .title {
@@ -122,9 +155,9 @@ export default {
 }
 
 .table-c {
-	width: 92%;
+	width: 93%;
 	height: 82%;
-	margin: 0 4%;
+	// margin: 0 4%;
 	// overflow: auto;
 }
 </style>
@@ -132,8 +165,12 @@ export default {
 .time-table-container {
 	.table-c {
 		background: transparent;
+
 		.el-table {
-      background-color: rgba(133, 133, 133, 0.1);
+			background-color: transparent;
+			&::before{
+				height: 0px !important;
+			}
 			tr {
 				color: #fff;
 			}
@@ -158,19 +195,67 @@ export default {
 			.el-table__cell {
 				padding: 6px 0 !important;
 				height: 30px;
-        font-size: 10px;
+				font-size: 10px;
 				border-bottom: none;
 			}
 		}
-    tr:hover > td {
-      background-color: rgba(133, 133, 133, 0.5) !important;
-      color: #0099ff;
-    }
+
+		// tr:hover>td {
+		// 	background-color: rgba(133, 133, 133, 0.5) !important;
+		// 	color: #0099ff;
+		// }
 	}
 }
 
 ::-webkit-scrollbar {
-  display: none;
+	display: none;
 }
+</style>
+<style lang="scss">
+$width: 10px;
+$width_active: 6px;
 
+.time-table-container {
+
+	.el-table__body-wrapper::-webkit-scrollbar {
+		width: 0;
+	}
+
+	.el-table__header-wrapper,
+	.el-table__body-wrapper {
+		padding-left: 7%
+	}
+
+
+	.el-table--enable-row-hover .el-table__body tr:hover {
+		td {
+			background-color: #3c4f72 !important;
+			color: #478ddb;
+
+			&:first-child {
+				content: "";
+				width: $width;
+
+
+				&::before {
+					content: "";
+					position: absolute;
+					top: 11px;
+					left: -12px;
+					width: 0;
+					height: 0;
+					border-bottom: $width_active solid transparent;
+					border-top: $width_active solid transparent;
+					border-left: $width_active solid #4da3ff;
+					border-right: $width_active solid transparent;
+
+				}
+
+			}
+
+
+		}
+
+	}
+}
 </style>
