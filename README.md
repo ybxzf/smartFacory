2c1dbf4316e31fdfcc6978fa2b9372fa13a20f9f
mujinzjh <1161223922@qq.com>
2024-11-15 23:28:00 +0800

    [feat]命名规范

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ npm run build:stage
 # 构建生产环境
 npm run build:prod
 ```
+
+## 统一命名规范
+- 文件名首字母大写，采用大驼峰式命名
+- 文件夹首字母小写，采用小驼峰式命名
+- 文件中如果存在`index.vue`，不做任何修改,就采用`index.vue`的命名方式
+- 公共工具方法抽取到`utils`文件夹下的`tools.js`文件中
--- src/views/index.vue
+++ src/views/index.vue
@@ -55,15 +55,15 @@
 </template>
 
 <script>
-import Workshop from './bigScreen/components/workshop'
-import StatusShow from './bigScreen/components/statusShow'
 import moment from 'moment';
-import CuboidBar from './bigScreen/components/cuboidBar'
-import Cylinder from './bigScreen/components/cylinder.vue';
-import lineAndStackBar from './bigScreen/components/lineAndStackBar.vue';
-import realTimeMonitor from "./bigScreen/components/realTimeMonitor.vue";
-import OrderScheduling from './bigScreen/components/orderScheduling.vue';
-import Attendance from './bigScreen/components/attendance.vue';
+import Workshop from './bigScreen/components/Workshop'
+import StatusShow from './bigScreen/components/StatusShow'
+import CuboidBar from './bigScreen/components/CuboidBar'
+import Cylinder from './bigScreen/components/Cylinder';
+import LineAndStackBar from './bigScreen/components/LineAndStackBar';
+import RealTimeMonitor from "./bigScreen/components/RealTimeMonitor";
+import OrderScheduling from './bigScreen/components/OrderScheduling';
+import Attendance from './bigScreen/components/Attendance';
 export default {
 	name: "Index",
 	components: {
@@ -71,8 +71,8 @@ export default {
     StatusShow,
 		CuboidBar,
 		Cylinder,
-		lineAndStackBar,
-		realTimeMonitor,
+		LineAndStackBar,
+		RealTimeMonitor,
 		OrderScheduling,
 		Attendance
 	},
