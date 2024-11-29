fe065da5dfd9a5c0a7feb935b7271e923c54a47d
ybxzf <517394139@qq.com>
2024-11-29 23:30:11 +0800

    dip2大致布局

--- src/views/bigScreenDip2/index.vue
+++ src/views/bigScreenDip2/index.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="">DIP二级看板</div>
+    <div class="dip-two-container">
+        <custom-header class="header-container">
+            DIP-02生产线可视化看板
+        </custom-header>
+        <custom-content class="content-container"></custom-content>
+
+    </div>
 </template>
 
 <script>
+import CustomHeader from './components/layout/header.vue';
+import CustomContent from './components/layout/content.vue';
+
 export default {
     name: "",
+    components: {
+        CustomHeader,
+        CustomContent,
+    },
     data() {
         return {
 
@@ -18,4 +31,28 @@ export default {
     },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dip-two-container {
+    width: 100%;
+    height: calc(100vh - 84px);
+    /* 设置背景图片 */
+    background-image: url('../../assets/images/bigScreen/bigScreen_bg.png');
+    /* 不重复背景图片 */
+    background-repeat: no-repeat;
+    /* 图片铺满容器 */
+    background-size: 100% 120%;
+    /* 背景居中显示 */
+    background-position: center center;
+    // background: #070f20;
+    // background-color: #409eff;
+    color: #ffffff;
+
+    .header-container {
+        margin: 0 10px;
+    }
+
+    .content-container {
+        margin: 10px;
+    }
+}
+</style>
